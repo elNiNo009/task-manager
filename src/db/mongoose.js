844139d3e89ee0eef5b957eabe82e123054a9e2b
sarthak chauhan <sarthak.chauhan496@gmail.com>
2022-04-01 00:39:02 +0530

    db update

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose')
-
+const validator=require('validator')
 mongoose.connect('mongodb://127.0.0.1:27017/task-manger-api',
 {
     useNewUrlParser:true,
@@ -7,19 +7,71 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manger-api',
 }
 )
 
+
 const User = mongoose.model('User',{
     name:{
-        type: String
+        type: String,
+        required:true,
+        trim:true
     },
     age:{
-           type: Number
+           type: Number,
+           default:0,
+           validate(value){
+               if(value<0)
+               {
+                throw new Error('age invalid')
+               }
+           }
+    },
+    email:{
+        type:String,
+        required:true,
+        trim:true,
+        lowercase:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Email invalid')
+            }
+        }
+    },
+    password:
+    {
+        type:String,
+        required:true,
+        trim:true,
+        minlength:7,
+        validate(value){
+            if(value.toLowerCase().includes('password'))
+            {
+                throw new Error('password cannot be password')
+            }
+        }
+
     }
 })
 
+const Task = mongoose.model('Tasks',{
+    Description:{
+        type: String,
+        trim:true,
+        required:true
+
+    },
+    Completed:{
+           type: Boolean,
+         //  required:true,
+           default:false
+    }
+})
+
+
+/*
 const me= new User(
     {
-        name:"sarthak",
-        age: 25
+        name:"satyam",
+        age: 28,
+        email:'sat@'
     }
 )
 
@@ -28,3 +80,19 @@ console.log("result",me)
 }).catch((error)=>{
     console.log("error",error)
 })
+
+
+
+const firstTask= new Task(
+    {
+        Description:"bath",
+        Completed: true
+    }
+)
+firstTask.save().then(()=>{
+    console.log("result",firstTask)
+    }).catch((error)=>{
+        console.log("error",error)
+    })
+
+    */
