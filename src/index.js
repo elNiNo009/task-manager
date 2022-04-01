76bc289896127c7843eb71af3ce3706c7646008f
sarthak chauhan <sarthak.chauhan496@gmail.com>
2022-04-01 15:08:55 +0530

    update endpoint

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ require('./db/mongoose')
 
 const User=require('./models/user')
 const Task=require('./models/task')
+const { findByIdAndUpdate } = require('./models/user')
 
 const app=express()
 const port=process.env.PORT || 3000
@@ -88,7 +89,7 @@ app.get('/tasks',async (req,res)=>{                //reading endpoint task many
 
 app.get('/tasks/:id', async(req,res)=>{           //reading endpoint task single
     const task_id=req.params.id
-   
+    console.log("reading")
         try{
               const tasks= await  Task.findById(task_id)
             if(!tasks)
@@ -103,6 +104,65 @@ app.get('/tasks/:id', async(req,res)=>{           //reading endpoint task single
             res.status(500).send(e)
         }
     
+})
+
+
+app.patch('/users/:id',async(req,res)=>{           //update endpoint user
+    
+    const updates=Object.keys(req.body)
+    const allowUpdates=['name','email','password','age']
+    const isValidOperation= updates.every((update)=>{
+          return allowUpdates.includes(update)
+    })
+    if(!isValidOperation)
+    {
+          return res.status(400).send({
+              error: 'Invlaid update'
+          })
+    }
+   
+    try{
+        const user= await User.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true})
+        if(!user)
+        {
+          
+            return res.status(404).send()
+        }
+        res.send(user)
+       }
+catch(e)
+{
+     res.status(400).send(e)
+}
+})
+
+app.patch('/tasks/:id',async(req,res)=>{           //update endpoint task
+    
+    const updates=Object.keys(req.body)
+    const allowUpdates=['Description','Completed']
+    const isValidOperation= updates.every((update)=>{
+          return allowUpdates.includes(update)
+    })
+    if(!isValidOperation)
+    {
+          return res.status(400).send({
+              error: 'Invlaid update'
+          })
+    }
+   
+    try{
+        const task= await Task.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true})
+        if(!task)
+        {
+          
+            return res.status(404).send()
+        }
+        res.send(task)
+       }
+catch(e)
+{
+     res.status(400).send(e)
+}
 })
 
 
