b81a414f99466e5dae2d555efa95009e3a8dd455
sarthak chauhan <sarthak.chauhan496@gmail.com>
2022-04-01 14:10:32 +0530

    async await

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,7 +5,8 @@ const mongoose=require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/task-manger-api',
 {
     useNewUrlParser:true,
-    useCreateIndex:true
+    useCreateIndex:true,
+    useFindAndModify:false
 }
 )
 
--- src/index.js
+++ src/index.js
@@ -1,96 +1,110 @@
 const express=require('express')
 require('./db/mongoose')
+
 const User=require('./models/user')
 const Task=require('./models/task')
 
 const app=express()
-
 const port=process.env.PORT || 3000
-
 app.use(express.json())
 
-app.post('/users',(req,res)=>{            //creation endpoit for user
 
+
+app.post('/users', async (req,res)=>{            //creation endpoit for user
+  
     const user=new User(req.body)
-    user.save().then(()=>{
-        res.status(201).send(user)
-    }).catch((error)=>{
+    try
+        {
+         await user.save()
+         res.status(201).send(user)
+        }
+    catch (error)
+    {
         res.status(400)
-            res.send(error)
-    })
-   
+        res.send(error)
+    }
 })
 
-app.post('/tasks',(req,res)=>{             //creation endpoint for task
+
+app.post('/tasks',async (req,res)=>{             //creation endpoint for task
 
     const task=new Task(req.body)
-    task.save().then(()=>{
-        res.send(task)
-    }).catch((error)=>{
-        res.status(400)
-            res.send(error)
-    })
+    try {
+       await task.save()
+       res.status(201).send(task)
+    }
+    catch(error)
+    {
+       res.status(400).send(error)
+    }
    
 })
 
-app.get('/users',(req,res)=>{               //reading endpoint user many
-    User.find({}).then((users)=>{
-        console.log("there")
 
-        res.send(users)
-
-    }).catch((e)=>{
-         res.status(500).send(e)
-    })
+app.get('/users',async (req,res)=>{               //reading endpoint user many
+    
+    try{
+    const users=await User.find({})
+    res.send(users)
+    }
+    catch(e)
+    {
+        res.status(500).send(e)
+    }
+   
 })
 
-app.get('/users/:id',(req,res)=>{           //reading endpoint user single
+
+app.get('/users/:id', async(req,res)=>{           //reading endpoint user single
     const user_id=req.params.id
-   // console.log("here I")
-    User.findById(user_id).then((users)=>{
-       // console.log("here")
-        if(!users)
-        {
-            
-            return res.status(404).send()
-        }
-        res.send(users)
-
-    }).catch((e)=>{
-         res.status(500).send(e)
-    })
+      try{
+         const users=await  User.findById(user_id)
+         if(!users)
+         {
+             
+             return res.status(404).send()
+         }
+         res.send(users)
+      }
+      catch(e)
+      {
+        res.status(500).send(e)
+      }
     
 })
 
 
-app.get('/tasks',(req,res)=>{                //reading endpoint task many
-    Task.find({}).then((tasks)=>{
-      //  console.log("there")
+app.get('/tasks',async (req,res)=>{                //reading endpoint task many
+    try{
 
+        const tasks=await Task.find({})
         res.send(tasks)
-
-    }).catch((e)=>{
-         res.status(500).send(e)
-    })
+    }catch(e)
+    {
+        res.status(500).send(e)
+    }
 })
 
-app.get('/tasks/:id',(req,res)=>{           //reading endpoint task single
+
+app.get('/tasks/:id', async(req,res)=>{           //reading endpoint task single
     const task_id=req.params.id
-   // console.log("here I")
-    Task.findById(task_id).then((tasks)=>{
-       // console.log("here")
-        if(!tasks)
-        {
-            
-            return res.status(404).send()
-        }
-        res.send(tasks)
+   
+        try{
+              const tasks= await  Task.findById(task_id)
+            if(!tasks)
+            {
+                
+                return res.status(404).send()
+            }
+            res.send(tasks)
 
-    }).catch((e)=>{
-         res.status(500).send(e)
-    })
+        }catch(e)
+        {
+            res.status(500).send(e)
+        }
     
 })
+
 
 app.listen(port, ()=>{
     console.log('Serevr is running '+ port)
