8b9dd6dc503af4cc704a1d0b9bd804fec0aeb006
sarthak chauhan <sarthak.chauhan496@gmail.com>
2022-04-02 21:54:54 +0530

    deleted task when user deleted

--- src/models/user.js
+++ src/models/user.js
@@ -3,7 +3,8 @@ const mongoose=require('mongoose')
 const validator=require('validator')
 const bcrypt =require('bcryptjs')
 const jwt=require('jsonwebtoken')
-
+//const tasks=require('')
+const Tasks = require('./task')
 
 const userSchema=new mongoose.Schema({          
     name:{
@@ -108,7 +109,11 @@ userSchema.pre('save', async function(next)
      next()
 
 })
-
+userSchema.pre('remove',async function(next){
+    const user=this
+    await Tasks.deleteMany({Owner:user._id})
+    next()
+})
 const User = mongoose.model('User',userSchema)
 
 module.exports=User
