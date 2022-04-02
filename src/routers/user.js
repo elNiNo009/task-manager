d0b33478b80c2acb6b5de9722ab7b1753aaa8397
sarthak chauhan <sarthak.chauhan496@gmail.com>
2022-04-02 20:14:39 +0530

    authetication completed user

--- src/routers/user.js
+++ src/routers/user.js
@@ -63,25 +63,7 @@ router.get('/users/me',auth,async (req,res)=>{               //reading endpoint 
    
 })
 
-router.get('/users/:id', async(req,res)=>{           //reading endpoint user single
-    const user_id=req.params.id
-      try{
-         const users=await  User.findById(user_id)
-         if(!users)
-         {
-             
-             return res.status(404).send()
-         }
-         res.send(users)
-      }
-      catch(e)
-      {
-        res.status(500).send(e)
-      }
-    
-})
-
-router.patch('/users/:id',async(req,res)=>{           //update endpoint user
+router.patch('/users/me',auth,async(req,res)=>{           //update endpoint user
     
     const updates=Object.keys(req.body)
     const allowUpdates=['name','email','password','age']
@@ -97,18 +79,14 @@ router.patch('/users/:id',async(req,res)=>{           //update endpoint user
    
     try{
 
-        const user=await User.findById(req.params.id)
+        //const user=await User.findById(req.params.id)
         updates.forEach((update)=>{
-            user[update]=req.body[update]
+            req.user[update]=req.body[update]
         })
-        await user.save()
+        await req.user.save()
         //const user= await User.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true})
-        if(!user)
-        {
-          
-            return res.status(404).send()
-        }
-        res.send(user)
+       
+        res.send(req.user)
        }
 catch(e)
 {
@@ -116,16 +94,19 @@ catch(e)
 }
 })
 
-router.delete('/users/:id',async(req,res)=>{          //delete endpoint user
+router.delete('/users/me',auth,async(req,res)=>{          //delete endpoint user
 
     try{
-         const user=await User.findByIdAndDelete(req.params.id)
+        //  const user=await User.findByIdAndDelete(req.user._id)
 
-         if(!user)
-         {
-             return res.status(404).send()
-         }
-         res.send(user)
+        //  if(!user)
+        //  {
+        //      return res.status(404).send()
+        //  }
+        //  res.send(user)
+
+        await req.user.remove()
+        res.send(req.user)
     }
     catch(e)
     {
